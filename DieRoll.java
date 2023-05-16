3f12cc72e5648a9a8a70691a2c8bc9210ea12206
PhanAnhHao <133492630+PhanAnhHao@users.noreply.github.com>
2023-05-16 11:05:03 +0700

    DieRoll.java

--- DieRoll.java
+++ DieRoll.java
@@ -1,3 +1,4 @@
+//False declaration pimport -> import
 import java.util.*;
 /*
 JDice: Java Dice Rolling Program
@@ -23,7 +24,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 public class DieRoll {
     private int ndice;
-//    private int nsides;
+////Missing data type declaration for variable
+    private int nsides;
     private int bonus;
     private static Random rnd;
     static{
@@ -36,13 +38,16 @@ public class DieRoll {
 	this.nsides=nsides;
 	this.bonus=bonus;
     }
-    public RollResult make Roll() {
+ //False declaration
+    public RollResult makeRoll() {
 	RollResult r=new RollResult(bonus);
 	for(int i=0;i<ndice;i++) {
 	    int roll=rnd.nextInt(nsides)+1;
-	    r,addResult(roll);
+//Wrong character "," -> "."
+	    r.addResult(roll);
 	}
-	returnr;
+//False declaration returnr -> return r
+	return r;
     }
     public String toString() {
 	String ans =ndice+"d"+nsides;
@@ -52,7 +57,9 @@ public class DieRoll {
 	else if(bonus<0) {
 	    ans=ans+bonus;
 	}
-	return ans
+//Missing accents ";"
+	return ans;
     }
 
 }
+
